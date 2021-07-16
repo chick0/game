02450a7f4b343d9184fdcc13ef66dfa2c0f13ddb
chick_0 <chick_0@ch1ck.xyz>
2021-07-17 05:35:29 +0900

    시나리오 선택 기능 추가

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 
 
 if __name__ == "__main__":
+    from app import VALUE
     from app.story import get_story
     from app.game import start
-    from app.story import get_story_list
+    from app.story import get_title
+    from app.story import get_playable_story_list
+    from app.event import get_event
 
-    story, item = get_story(name="nuclear")
+    story_map = {}
+    for this in get_playable_story_list():
+        story_map[get_title(this)] = this
+
+    VALUE['name'] = story_map[get_event("player.choice")(list(story_map.keys()))]
+
+    story, item = get_story(name=VALUE['name'])
     start(story=story, item=item)
