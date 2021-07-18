1c32605cf6ab12cdc8199f32233d19f5be140b60
chick_0 <chick_0@ch1ck.xyz>
2021-07-18 10:55:32 +0900

    명령어 개편

--- nuclear/command.py
+++ nuclear/command.py
@@ -1,21 +1,90 @@
 def show_inventory():
+    """
+    플레이어의 인벤토리를 확인하는 명령어
+    """
+    from . import VALUE
     print("[플레이어의 인벤토리]")
+    for n, i in VALUE['items'].items():
+        print(f"  {n}: {i}개")
+
+
+def load():
+    """
+    게임의 진행상황을 불러오는 명령어
+    """
+    from os import path
+    from json import load
+
+    from . import FLAG
+    from . import VALUE
+
+    filename = input("[?].json : ") + ".json"
+    try:
+        json = load(
+            fp=open(path.join(path.dirname(path.dirname(__file__)), filename), mode="r", encoding="utf8")
+        )
+
+        for i in range(0, len(FLAG)):
+            del FLAG[list(FLAG.keys())[0]]
+        FLAG.update(json['flag'])
+
+        for i in range(0, len(VALUE)):
+            del VALUE[list(VALUE.keys())[0]]
+        VALUE.update(json['value'])
+
+        print(f"** 게임을 불러왔습니다 : {filename} **")
+    except (FileNotFoundError, Exception) as e:
+        print(f"** 게임을 불러오지 못함:{e.__class__.__name__} **")
+        print(e)
+
+
+def save():
+    """
+    게임의 진행상황을 저장하는 명령어
+    """
+    from os import path
+    from os import urandom
+    from json import dump
+
+    from . import FLAG
+    from . import VALUE
+
+    filename = urandom(2).hex() + ".json"
+    dump(
+        obj={
+            "flag": FLAG,
+            "value": VALUE
+        },
+        fp=open(path.join(path.dirname(path.dirname(__file__)), filename), mode="w", encoding="utf8")
+    )
+    print(f"** 게임이 저장되었습니다 : {filename} **")
+
+
+def help_():
+    """
+    사용가능한 명령어를 보여주는 명령어
+    """
+    for key in __command__.keys():
+        print(key, __command__[key].__doc__)
 
 
 def debug():
-    from nuclear import FLAG
-    from nuclear import VALUE
+    """
+    [개발용] 변수를 확인하는 명령어
+    """
+    from . import FLAG
+    from . import VALUE
     print(FLAG)
     print(VALUE)
 
 
 __command__ = {
-    "인벤토리": show_inventory,
-    "inventory": show_inventory,
+    "e": show_inventory,
+
+    "help": help_,
+
+    "save": save,
+    "load": load,
 
     "debug": debug
 }
-
-# do not touch this
-__all__ = [name for name in dir() if not name.startswith("_")]
-# do not touch this
