d2df65d060c8f8046e8eb40093e31f7061dfb544
chick_0 <chick_0@ch1ck.xyz>
2021-07-20 01:27:32 +0900

    TODO

--- nuclear/event/snack_bar_no2.py
+++ nuclear/event/snack_bar_no2.py
@@ -11,4 +11,8 @@ event_id = __name__
 def start():
     print_title(title=title)
     # TODO:파밍지 만들기
+    # TODO:주방           snack_bar_no2_kitchen
+    # TODO:무인계산기     snack_bar_no2_kiosk
+    # TODO:냉장고         snack_bar_no2_refrigerator
+    # TODO:1인용 식탁     snack_bar_no2_table
     return None
