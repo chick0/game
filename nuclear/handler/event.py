edef297c51c1867d42518fe0423a7ff9d9bbbbbf
chick_0 <chick_0@ch1ck.xyz>
2021-07-20 01:26:25 +0900

    이벤트 아이디를 불러오는 핸들러 추가

--- nuclear/handler/event.py
+++ nuclear/handler/event.py
@@ -1,2 +1,6 @@
 def get(name: str):
     return getattr(getattr(getattr(__import__(f"nuclear.event.{name}"), "event"), name), "start")
+
+
+def get_event_id(name: str):
+    return getattr(getattr(getattr(__import__(f"nuclear.event.{name}"), "event"), name), "event_id")
