f2944248ba39cea9195d7e7d65c7bc5ff275dabe
chick_0 <chick_0@ch1ck.xyz>
2021-07-21 05:09:59 +0900

    병원 디자인

--- nuclear/event/hospital.py
+++ nuclear/event/hospital.py
@@ -10,5 +10,15 @@ event_id = __name__
 
 def start():
     print_title(title=title)
-    # TODO:파밍지 만들기
+
+    # TODO:1층               hospital_1f
+    # TODO:1층 안내데스크    hospital_1f_info
+    # TODO:1층 화장실        hospital_1f_bathroom
+    # TODO:1층 약국          hospital_1f_pharmacy
+
+    # TODO:2층               hospital_2f
+    # TODO:2층 원장실        hospital_2f_doctor
+    # TODO:2층 진료실        hospital_2f_doctor_office
+    # TODO:2층 입원실 A      hospital_2f_ward_a
+    # TODO:2층 입원실 B      hospital_2f_ward_b
     return None
