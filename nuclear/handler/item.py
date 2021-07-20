4b1f152ba4056716032457c33f1d1bf3ea61b166
chick_0 <chick_0@ch1ck.xyz>
2021-07-21 04:55:44 +0900

    아이템 추가 핸들러 업데이트

--- nuclear/handler/item.py
+++ nuclear/handler/item.py
@@ -1,9 +1,13 @@
 
 from nuclear import VALUE
+from nuclear.items import items
 
 
 def get(item_name: list):
     for this in item_name:
+        if this not in list(items.keys()):
+            raise KeyError("등록되지 않은 아이템을 사용자의 인벤토리에 추가하려 했습니다.")
+
         print(f"+1 {this}")
         try:
             VALUE['items'][this] += 1
