abd96b3458266820690285aed2557e2b5ab3f1b7
chick_0 <chick_0@ch1ck.xyz>
2021-07-20 21:22:30 +0900

    식당 업데이트

--- nuclear/event/snack_bar_counter.py
+++ nuclear/event/snack_bar_counter.py
@@ -15,9 +15,8 @@ def start():
     else:
         flag.update(event_id, True)
 
-    print_message(text="핵이 터지면 돈이 더 이상 의미가 없음을 알면서도 "
-                       "사람들은 이미 계산데에 있는 돈을 훔쳐간 것 같습니다.")
-    print_message(text="그래도 구석에 있는 사탕 하나를 발견했다.")
+    print_message(text="돈이 의미가 없음을 알면서도 이미 계산대에 있는 돈을 훔쳐 간 것 같습니다.")
+    print_message(text="그래도 구석에 있는 사탕 하나를 발견했습니다.")
     item.get(["사탕"])
 
     return
--- nuclear/event/snack_bar_kitchen.py
+++ nuclear/event/snack_bar_kitchen.py
@@ -9,35 +9,35 @@ event_id = __name__
 
 
 def event_a():
-    print_message(text="냉장고에서 토마토 수프를 발견했습니다.")
+    print_message(text="냉장고에서 토마토 수프를 발견했다.")
     item.get(["토마토 수프"] * random.choice([1, 1, 1, 2, 2, 3, 4]))
 
 
 def event_b():
-    print_message(text="버려진 사탕을 발견했습니다.")
-    print_message(text="또 그 사탕의 옆에 한 메모가 있었습니다.")
+    print_message(text="버려진 사탕을 발견했다.")
+    print_message(text="또 그 사탕의 옆에 한 메모가 있었다.")
     print_message(text='"인터넷에서 사탕을 2Kg 사는 것은 나쁜 생각이다..."')
     item.get(["사탕"] * random.choice([3, 3, 3, 4, 5, 6]))
 
 
 def event_c():
-    print_message(text="수건을 발견했습니다.")
+    print_message(text="수건을 발견했다.")
     item.get(["수건"])
 
 
 def event_d():
     print_message(text="당신보다 빨리 이 식당에 온 사람이 있었나 봅니다.")
-    print_message(text="수납장에는 아무것도 없었습니다.")
+    print_message(text="수납장에는 아무것도 없었다.")
 
 
 def event_e():
-    print_message(text="환풍기는 고장이 난 듯 아무런 반응이 보이지 않습니다.")
+    print_message(text="환풍기는 고장 난 듯 아무런 반응을 하지 않습니다.")
 
 
 def start():
     print_title(title=title)
     for text in [
-        "주방 구석에 조그만한 불씨가 보인다.",
+        "주방 구석에 조그만한 불씨가 보입니다.",
         "여러 곳을 조사하기에는 시간이 부족해보인다."
     ]:
         print_message(text=text)
@@ -55,6 +55,6 @@ def start():
     choice = player.choice(list(ev_map.keys()))
     ev_map[choice]()
 
-    print_message(text="그때 조그만한 불씨가 당신을 위협합니다.")
+    print_message(text="그때 커져 버린 불씨가 당신을 위협합니다.")
 
     return None
