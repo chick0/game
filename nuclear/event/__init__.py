656385e5f81c79889158d7199d8cd5ca15a20745
chick_0 <chick_0@ch1ck.xyz>
2021-07-23 14:14:47 +0900

    출력 함수 수정

--- nuclear/event/__init__.py
+++ nuclear/event/__init__.py
@@ -4,15 +4,19 @@ from hashlib import sha1
 from nuclear import VALUE
 
 
-def print_title(title: str):
-    print(f"\n\n\n[ {title} ]")
+def print_title(title: str, no_warp: bool = False):
+    if not no_warp:
+        print("\n\n")
+
+    print(f"[ {title} ]")
 
 
-def print_message(text: str, no_wait: bool = False):
-    print("  ", text)
+def print_message(text: str, no_wait: bool = False, no_indent: bool = False):
+    indent = "   " if no_indent is False else ""
+    print(indent, text, sep="")
 
-    text_id = sha1(text.encode()).hexdigest()
-    if not VALUE['event'].get(text_id, None):
-        VALUE['event'][text_id] = len(text)
-        if not no_wait:
+    if not no_wait:
+        text_id = sha1(text.encode()).hexdigest()
+        if not VALUE['event'].get(text_id, None):
+            VALUE['event'][text_id] = len(text)
             sleep(0.65)
