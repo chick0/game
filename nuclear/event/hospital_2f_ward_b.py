cbc7651484a1a0d694f4e479553b5310d9c02c5e
chick_0 <chick_0@ch1ck.xyz>
2021-07-26 02:37:10 +0900

    병원 2층 업데이트 - 병원 2층 입원실 B

--- nuclear/event/hospital_2f_ward_b.py
+++ nuclear/event/hospital_2f_ward_b.py
@@ -10,5 +10,20 @@ event_id = __name__
 
 def start():
     print_title(title=title)
+    if flag.check(event_id, True):
+        return
+    else:
+        flag.update(event_id, True)
+
+    for text in [
+        "비어있는 병실에서 아무것도 찾을 수 없습니다.",
+        "심지어 8인실인데도 어떤 물건도 남아있지 않습니다.",
+        "당신보다 이 입원실에 빨리 온 사람이 있었나 봅니다.",
+        "벽 구석에 작은 낙서가 적혀있습니다..",
+        '"우리는 왜 휴게실이 없을까?"',
+        "그 옆에 다른 글씨체가 답장을 적었습니다.",
+        '"그러게요..."'
+    ]:
+        print_message(text=text)
 
     return None
