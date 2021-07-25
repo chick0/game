3c7548da0511492d532fdb0ea1288943567e41d2
chick_0 <chick_0@ch1ck.xyz>
2021-07-26 01:35:17 +0900

    아이템 명령어 추가

--- nuclear/command.py
+++ nuclear/command.py
@@ -3,11 +3,27 @@ def show_inventory():
     플레이어의 인벤토리를 확인하는 명령어
     """
     from . import VALUE
-    from .event import print_title
-    from .event import print_message
-    print_title(title="플레이어의 인벤토리", no_warp=True)
+    print("[ 플레이어의 인벤토리 ]")
     for n, i in VALUE['items'].items():
-        print_message(text=f"{n}: {i}개", no_wait=True)
+        print(f"- {n}: {i}개")
+
+
+def item():
+    """
+    아이템의 세부정보를 확인하는 명령어
+    """
+    from . import VALUE
+    from .items import items
+    from .handler import player
+
+    keys = list(VALUE['items'].keys())
+    if len(keys) >= 1:
+        print("[ 세부정보를 확인하고 싶은 아이템을 선택해주세요 ]")
+
+        key = player.choice(keys)
+        print(f"{key}: {items[key]['explain']}")
+    else:
+        print("보유하고 있는 아이템이 없습니다.")
 
 
 def help_():
@@ -30,6 +46,7 @@ def debug():
 
 __command__ = {
     "e": show_inventory,
+    "i": item,
 
     "help": help_,
 
